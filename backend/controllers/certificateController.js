be7207667187a4f92c7611b56506016e1016dd56
Harsh Agrawal <harshagrawal4256@gmail.com>
2026-06-19 11:25:38 +0545

    updates certification section

--- backend/controllers/certificateController.js
+++ backend/controllers/certificateController.js
@@ -7,6 +7,10 @@ export const getCertificates = async (req, res) => {
 
 export const addCertificate = async (req, res) => {
     try {
+        const { order } = req.body;
+        if (order && order > 0) {
+            await Certificate.updateMany({ order: { $gte: order } }, { $inc: { order: 1 } });
+        }
         const certificate = new Certificate(req.body);
         await certificate.save();
         res.status(201).json(certificate);
@@ -34,6 +38,13 @@ export const deleteCertificate = async (req, res) => {
 export const updateCertificate = async (req, res) => {
     try {
         const { id } = req.params;
+        const { order } = req.body;
+        if (order && order > 0) {
+            await Certificate.updateMany(
+                { _id: { $ne: id }, order: { $gte: order } },
+                { $inc: { order: 1 } }
+            );
+        }
         const updatedCertificate = await Certificate.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
         
         if (!updatedCertificate) {
